754337e59650286b5ead338439c39cf6abee7974
Ga-ol-an <gabrielolandrade43@gmail.com>
2024-05-22 19:19:49 -0300

    Add more results

--- fourth_attempt/results/QoS_traffic_assimetric/sem_qos/video_20s_depois_5s/clients.sh
+++ fourth_attempt/results/QoS_traffic_assimetric/sem_qos/video_20s_depois_5s/clients.sh
@@ -1,4 +1,6 @@
 h1 iperf -c 10.0.0.4 -p 5001 -u -b 10M -t 30 -e &
 h2 iperf -c 10.0.0.4 -p 5002 -u -b 10M -t 30 -e &
+px import time
+px time.sleep(5)
 h3 iperf -c 10.0.0.4 -p 5003 -u -b 10M -t 20 -e &
 
