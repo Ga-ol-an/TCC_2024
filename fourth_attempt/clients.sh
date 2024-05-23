aa53fa0adc7ee1f6efceec560fecff25eec966d4
Ga-ol-an <gabrielolandrade43@gmail.com>
2024-05-22 22:36:29 -0300

    Add firts results obtained

--- fourth_attempt/clients.sh
+++ fourth_attempt/clients.sh
@@ -1,4 +1,4 @@
 h1 iperf -c 10.0.0.4 -p 5001 -u -b 10M -t 30 -e &
 h2 iperf -c 10.0.0.4 -p 5002 -u -b 10M -t 30 -e &
-h3 iperf -c 10.0.0.4 -p 5003 -u -b 10M -t 20 -e &
+h3 iperf -c 10.0.0.4 -p 5003 -u -b 10M -t 30 -e &
 
