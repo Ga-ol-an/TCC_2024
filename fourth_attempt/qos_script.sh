bf030723db1bfbe47fdb40e94130e6a06f8300e6
Ga-ol-an <gabrielolandrade43@gmail.com>
2024-05-23 16:22:05 -0300

    Add more results

--- fourth_attempt/qos_script.sh
+++ fourth_attempt/qos_script.sh
@@ -12,8 +12,8 @@ sleep .5 # Waits 0.5 second.
 curl -X POST -d '{"priority": "1","match": { "tp_dst": "5001" ,"nw_proto": "UDP", "dl_type":"IPv4"}, "actions": {"queue": "0"}}' http://localhost:8080/qos/rules/0000000000000001 
 echo 
 sleep .5 # Waits 0.5 second.
-curl -X POST -d '{"priority": "2","match": { "tp_dst": "5002" ,"nw_proto": "UDP", "dl_type":"IPv4"}, "actions": {"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001 
+curl -X POST -d '{"priority": "1","match": { "tp_dst": "5002" ,"nw_proto": "UDP", "dl_type":"IPv4"}, "actions": {"queue": "1"}}' http://localhost:8080/qos/rules/0000000000000001 
 echo 
 sleep .5 # Waits 0.5 second.
-curl -X POST -d '{"priority": "3","match": { "tp_dst": "5003" ,"nw_proto": "UDP", "dl_type":"IPv4"}, "actions": {"queue": "2"}}' http://localhost:8080/qos/rules/0000000000000001 
+curl -X POST -d '{"priority": "1","match": { "tp_dst": "5003" ,"nw_proto": "UDP", "dl_type":"IPv4"}, "actions": {"queue": "2"}}' http://localhost:8080/qos/rules/0000000000000001 
 echo
