aab0fa33887325cb3c3aabf9b0d7fd75e7551c80
Ga-ol-an <gabrielolandrade43@gmail.com>
2024-06-07 04:15:26 -0300

    more advances on project

--- fourth_attempt/real_scen.sh
+++ fourth_attempt/real_scen.sh
@@ -12,12 +12,11 @@ h4 iperf -s -u -i 1 -p 5003 -e > outputs/realistic/h4_server_h3 &
 px time.sleep(1)
 
 px print('Begining clients')
-h1 iperf -c 10.0.0.4 -p 5001 -u -b 10M -t 10 -e & 
+h1 iperf -c 10.0.0.4 -p 5001 -u -b 10M -t 15 -e &
 px time.sleep(5)
-h2 iperf -c 10.0.0.4 -p 5002 -u -b 10M -t 30 -e & 
-px time.sleep(10)                                 
-h3 iperf -c 10.0.0.4 -p 5003 -u -b 10M -t 40 -e & 
+h2 iperf -c 10.0.0.4 -p 5002 -u -b 10M -t 35 -e &
+px time.sleep(15)                                
+h3 iperf -c 10.0.0.4 -p 5003 -u -b 10M -t 35 -e &
 px time.sleep(10)
-h1 iperf -c 10.0.0.4 -p 5001 -u -b 10M -t 20 -e & 
-px time.sleep(10)
+h1 iperf -c 10.0.0.4 -p 5001 -u -b 10M -t 20 -e &
 
